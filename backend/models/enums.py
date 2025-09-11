bce53afd89b621027f82b301a06d276797376051
Alfredo Liardo <alfredo.liardo@gmail.com>
2025-09-11 22:11:50 +0200

    update

--- backend/models/enums.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum, auto
-
-class PlayerRole(Enum):
-    GOALKEEPER = P = Gk = Pt = auto()
-    DEFENDER = D = Df = auto()
-    MIDFIELDER = C = Md = Cc = auto()
-    FORWARD = A = Fw = auto()
